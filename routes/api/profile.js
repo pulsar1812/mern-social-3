0bb6ca9866a034d307f5af625d9a4de1d8efd3b5
Patrick Lee <cluster000@gmail.com>
2019-10-23 17:58:19 +0800

    Finish posts route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -369,7 +369,7 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 router.get('/github/:username', (req, res) => {
   try {
     const options = {
-      uri: `https://api/github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${keys.githubClientId}&client_secret=${keys.githubSecret}`,
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${keys.githubClientId}&client_secret=${keys.githubSecret}`,
       method: 'GET',
       headers: { 'user-agent': 'node.js' }
     };
