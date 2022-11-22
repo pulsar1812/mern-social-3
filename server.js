2fdd91b13217c327345573d68baad79b06290b3a
pulsar1812 <cluster000@gmail.com>
2022-11-22 14:00:41 +0800

    Modify comment on server.js

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
-const connectDB = require('./config/db');
+const connectDB = require('./config/db')
 
-const app = express();
+const app = express()
 
 // Connect Database
-connectDB();
+connectDB()
 
-// Init Middleware
-app.use(express.json({ extended: false }));
+// Body Parser
+app.use(express.json({ extended: false }))
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 // Serve static API docs content
-app.use('/api-docs', express.static('./docs'));
+app.use('/api-docs', express.static('./docs'))
 
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build'))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
